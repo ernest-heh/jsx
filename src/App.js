cd056a1220cb611d73864b8abf5897d169d33c01
Ernest Ho <ernest.heh@gmail.com>
2023-04-11 13:47:03 +0800

    extract App component into App.js file

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <textarea
+      spellCheck
+      style={{ border: "3px solid #dedede", borderRadius: "10px" }}
+    />
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // 1. Import the React and ReactDOM Libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Import App component
+import App from "./App";
 
 // 2. Get a Reference to the div with ID root
 const element = document.getElementById("root");
@@ -8,10 +10,5 @@ const element = document.getElementById("root");
 // 3. Tell React to take control of that element
 const root = ReactDOM.createRoot(element);
 
-// 4. Create a component
-function App() {
-  return <h1>Hi there!</h1>;
-}
-
-// 5. Show the component on the screen
+// 4. Show the component on the screen
 root.render(<App />);
